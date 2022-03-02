d856ed6396a345fdf9eb345dc20378d7fe7c38c7
hemil0102 <hemil0102@gmail.com>
2022-03-02 21:07:35 +0900

    first commit

---
+++ bmi note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bmi note
+//
+//  Created by YEHROEI HO on 2022/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
