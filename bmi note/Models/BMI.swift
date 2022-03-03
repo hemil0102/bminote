223cbc390afc1ca976049d39064cf89daa944821
hemil0102 <hemil0102@gmail.com>
2022-03-03 19:57:32 +0900

    Walter님꺼 반영

---
+++ bmi note/Models/BMI.swift
@@ -0,0 +1,22 @@
+//
+//  Bminote.swift
+//  bmi note
+//
+//  Created by YEHROEI HO on 2022/03/03.
+//
+
+import Foundation
+
+struct Bminote {
+    let name: String
+    var age: Int
+    let gender: String
+    var profileImg: String
+    var height: String
+    var quote: String
+    let heightForBMI: Float
+    let weightForBMI: Float
+    let genderForBMI: Float
+    let regDate: String
+    let bmi: Float
+}
---
+++ bmi note/Models/BMIBrain.swift
@@ -0,0 +1,12 @@
+//
+//  BMIBrain.swift
+//  bmi note
+//
+//  Created by YEHROEI HO on 2022/03/03.
+//
+
+import Foundation
+
+struct BMIBrain {
+    
+}
