0662045d0bef97d61eb4240451a98b5f1402e0c1
Walter-Jo <ys.mobility@gmail.com>
2022-03-06 19:19:25 +0900

    tableView update

--- bmi note/Controllers/HistoryListVC.swift
+++ bmi note/Controllers/HistoryListVC.swift
@@ -13,10 +13,12 @@ class HistoryListVC: UIViewController {
     
     //목 데이터
     let mokListData = [
-        [26.5, "정상", 172.4, 62.1],
-        [31.3, "과체중", 162.9, 99.6],
-        [19.7, "저체중", 192.1, 45.2]
+        [26.5, "정상", 172.4, 62.1], [31.3, "과체중", 162.9, 99.6], [19.7, "저체중", 192.1, 45.2]
     ]
+    
+    //지역변수로 사용할 HistoryData
+    var receivedData:BMIBrain?          //???
+    var historyData:BMIBrain?           //???
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,14 +26,13 @@ class HistoryListVC: UIViewController {
         /*
          Todo
          1. XIB 커스텀 셀 만들기 [✓]
-         2. UserDefault.standard 로 저장된 값 가져오기 []
-         3. 데이터 가져와서 셋팅하기 []
-         4. 항목 선택시 뷰 이동 []
-         5. 선택된 항목 데이터 전달 []
+         2. 데이터 받아와서 셋팅하기 [✓]
+         3. 항목 선택시 뷰 이동 [✓]
+         4. 선택된 항목 데이터 전달 [✓]
          */
         
-        //TableView에 XIB 커스텀 셀 연결
-        setTableViewXIBCell()
+        setTableViewXIBCell()                   //TableView에 XIB 커스텀 셀 연결
+        setHistoryDataFromPrivousScreen()       //historyData에 전달받은 객체 대입
         
         //NavigationController 뒤로가기 한글로, MainVC 에서 바꿀 것
 //        self.navigationController?.navigationBar.topItem?.title = "메인"
@@ -45,17 +46,29 @@ class HistoryListVC: UIViewController {
         self.tableView.register(UINib(nibName: Constants.historyListCell, bundle: nil), forCellReuseIdentifier: Constants.historyListCellIdentifier)
     }
     
+    //전 화면에서 받아온 데이터를 로컬변수로 대입
+    func setHistoryDataFromPrivousScreen() {
+        if let data = receivedData {
+            historyData = data
+        }
+    }
+    
     //이력 삭제
     func deleteHistory() {
         /*
-         1. BMI 데이터 삭제
-         2. TableView Reload
+         1. BMI 데이터 삭제 []
+         2. UserDefault 업데이트 []
+         2. TableView Reload [✓]
          */
+
+//        self.tableView.reloadData()
     }
 }
 
 //TableView Delegate
 extension HistoryListVC: UITableViewDelegate {
+    
+    //항목 선택시 상세화면으로 이동
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -79,7 +92,10 @@ extension HistoryListVC: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.historyListCellIdentifier) as! HistoryListCell
         
         //Cell 안의 View에 데이터 셋팅하기
-        let row = indexPath.row
+        let row = indexPath.row     //항목 Index
+        
+        //historyData에서 값 꺼내기 []
+        //bim값에 따른 배경색 변경 []
         
         cell.bmiValue.text = "\(mokListData[row][0])"       //BMI 수치
         cell.bmiState.text = "\(mokListData[row][1])"       //BMI 상태
