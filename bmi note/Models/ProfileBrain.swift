7736840ec80ba1960138a93bc7fa0e4248ba9e1e
hemil0102 <hemil0102@gmail.com>
2022-03-09 18:16:10 +0900

    초기 프로파일 설정 80% 구현 완료

--- bmi note/Models/ProfileBrain.swift
+++ bmi note/Models/ProfileBrain.swift
@@ -9,7 +9,7 @@ import Foundation
 
 //Profile 저장과 로직을 위한 ViewModel
 struct ProfileBrain {
-    var myProfile = Profile(name: "Movel", age: 1, gender: "남", profileImg: "logo", height: 180.2, weight: 72.4, quote: "날아올라")
+    var myProfile = Profile(name: "Movel", age: 1, gender: "남", profileImg: "logo", height: 180.3, weight: 72.2, quote: "날아올라")
     
     // 세그먼트에서 성별을 입력 받는 메서드
     func getGenderType(selectedIndexTitle: String ) -> String {
